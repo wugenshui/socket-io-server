5aabc847f576761dd44c4102ed95b37650dcb926
chenbo <963838470@qq.com>
2022-06-19 11:04:14 +0800

    自定义socket id

--- index.js
+++ index.js
@@ -8,23 +8,30 @@ const io = new Server(httpServer, {
   },
 });
 
+io.use((socket, next) => {
+  // 当传输token时，使用token作为id
+  if (socket.handshake.auth.token) {
+    socket.id = socket.handshake.auth.token;
+  }
+  next();
+});
+
 // 监听新连接
 io.on("connection", (socket) => {
   console.log(
-    `new client connection: ${socket.id},current count: ${io.engine.clientsCount}`
+    `new user connection: ${socket.id}, current user count: ${io.engine.clientsCount}`
   );
 
   // 监听加入房间事件
   socket.on("join-room", (data) => {
     socket.join(data);
-    console.log(`${socket.id} join-room,in room: `, socket.rooms);
+    console.log(`join-room: ${socket.id} , room list: `, socket.rooms);
   });
 
   // 监听消息广播事件
   socket.on("send", (data, callback) => {
     // 通知房间用户
     socket.to(data.room).emit(data.data);
-    console.log("send", socket.id);
     // 广播消息回调，用于确认服务端是否收到消息
     if (typeof callback === "function") {
       callback("ok");
