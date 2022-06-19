a754323c013bf60ddea19fdc2e9188d5e00a6150
chenbo <963838470@qq.com>
2022-06-19 11:49:01 +0800

    日志+细节微调

--- index.js
+++ index.js
@@ -19,27 +19,33 @@ io.use((socket, next) => {
 // 监听新连接
 io.on("connection", (socket) => {
   console.log(
-    `new user connection: ${socket.id}, current user count: ${io.engine.clientsCount}`
+    `connection: ${socket.id}, user count: ${io.engine.clientsCount}`
   );
 
   // 监听加入房间事件
-  socket.on("join-room", (data) => {
-    socket.join(data);
-    console.log(`join-room: ${socket.id} , room list: `, socket.rooms);
-  });
-
-  // 监听消息广播事件
-  socket.on("send", (data, callback) => {
-    // 通知房间用户
-    socket.to(data.room).emit(data.data);
+  socket.on("join-room", (roomName, callback) => {
+    socket.join(roomName);
     // 广播消息回调，用于确认服务端是否收到消息
     if (typeof callback === "function") {
-      callback("ok");
+      callback({ state: true, room: [...socket.rooms] });
     }
   });
 
+  // 监听消息广播事件
+  socket.on("send", (data) => {
+    // 添加消息传输人信息
+    if (data && data.msg) {
+      data.msg.from = socket.id;
+    }
+    // 分发消息
+    socket.to(data.room).emit(data.msg);
+  });
+
+  // 监听掉线事件
   socket.on("disconnect", (reason) => {
-    console.log(`user disconnect: ${socket.id}, reason: ${reason}`);
+    console.log(
+      `disconnect: ${socket.id}, reason: ${reason}, user count: ${io.engine.clientsCount}`
+    );
   });
 });
 
